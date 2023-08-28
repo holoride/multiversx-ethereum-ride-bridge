cf0696b92acc4dafb76d896938706d646463f0e2
Tadhg Riordan <riordant@tcd.ie>
2023-08-28 15:53:25 +0100

    Add release build architecture; build with overflow checks

---
+++ rs/build-wasm-release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# builds all wasm targets
+
+set -e
+SMART_CONTRACT_JSONS=$(find . -name "multiversx.json")
+for smart_contract_json in $SMART_CONTRACT_JSONS
+do
+    smart_contract_folder=$(dirname $smart_contract_json)
+    echo ""
+    (set -x; cd $smart_contract_folder; cd meta; cargo +nightly run build --release; cd ../../)
+done
