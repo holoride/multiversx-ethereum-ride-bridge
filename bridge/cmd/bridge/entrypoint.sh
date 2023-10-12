c07bd668f21f88791895bcce482b51612a00d3a1
Florian Nücke <florian@holoride.com>
2023-10-12 16:57:52 +0200

    use character not used in our config vars for sed.

--- bridge/cmd/bridge/entrypoint.sh
+++ bridge/cmd/bridge/entrypoint.sh
@@ -26,35 +26,35 @@ then
   echo "\$ETH_NETWORK_ADDRESS is empty"
   exit 1
 fi
-sed -i -e "s/\$ETH_NETWORK_ADDRESS/$ETH_NETWORK_ADDRESS/g" ./config/config.toml
+sed -i -e "s|\$ETH_NETWORK_ADDRESS|$ETH_NETWORK_ADDRESS|g" ./config/config.toml
 
 if [ -z "$ETH_CONTRACT_ADDRESS" ]
 then
   echo "\$ETH_CONTRACT_ADDRESS is empty"
   exit 1
 fi
-sed -i -e "s/\$ETH_CONTRACT_ADDRESS/$ETH_CONTRACT_ADDRESS/g" ./config/config.toml
+sed -i -e "s|\$ETH_CONTRACT_ADDRESS|$ETH_CONTRACT_ADDRESS|g" ./config/config.toml
 
 if [ -z "$MVX_NETWORK_ADDRESS" ]
 then
   echo "\$MVX_NETWORK_ADDRESS is empty"
   exit 1
 fi
-sed -i -e "s/\$MVX_NETWORK_ADDRESS/$MVX_NETWORK_ADDRESS/g" ./config/config.toml
+sed -i -e "s|\$MVX_NETWORK_ADDRESS|$MVX_NETWORK_ADDRESS|g" ./config/config.toml
 
 if [ -z "$MVX_CONTRACT_ADDRESS" ]
 then
   echo "\$MVX_CONTRACT_ADDRESS is empty"
   exit 1
 fi
-sed -i -e "s/\$MVX_CONTRACT_ADDRESS/$MVX_CONTRACT_ADDRESS/g" ./config/config.toml
+sed -i -e "s|\$MVX_CONTRACT_ADDRESS|$MVX_CONTRACT_ADDRESS|g" ./config/config.toml
 
 if [ -z "$MVX_VALIDATOR_ADDRESS" ]
 then
   echo "\$MVX_VALIDATOR_ADDRESS is empty"
   exit 1
 fi
-sed -i -e "s/\$MVX_VALIDATOR_ADDRESS/$MVX_VALIDATOR_ADDRESS/g" ./config/config.toml
+sed -i -e "s|\$MVX_VALIDATOR_ADDRESS|$MVX_VALIDATOR_ADDRESS|g" ./config/config.toml
 
 
 echo "Starting bridge..."
